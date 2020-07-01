9f6f6f01cc215360576cf05353550120bbf281dc
gr3y6h0st <jayvy19@gmail.com>
2020-06-30 20:10:03 -0700

    uncommented androitTest file SleepDataBaseTest to test new SleepDatabase.

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,38 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object{
+        @Volatile //helps makes sure value is always update and same to all threads
+        private var INSTANCE: SleepDatabase? = null
+        
+        //Uses Singleton Principle
+        fun getInstance(context: Context) : SleepDatabase {
+            //synchronized = one thread of execution at a time
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
